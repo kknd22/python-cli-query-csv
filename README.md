ade7216fd634e51118b478d7447cf4db1e30d506
kknd22 <chris.lin@mail.com>
2021-12-12 16:22:26 -0500

    initial check in

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # python-cli-query-csv
+Python CLI to Query Food Truck Data in CVS File
+
+i## Overview
+The code illustrate a simple Python 3 based CLI program to query a SF food truck data in CSV format.
+
+## Prerequisite
+Python 3 
+
+## Usage
+unpack the 2 files main.py and ft.csv in a directory
+run `python main.py`
+should see this prompts:
+``` 
+Use the following format to query food truck data (case insensetive):
+search format: field=value 
+at present only support these seven searchable fields: name, type, location, address, status, food, zip
+
+example: name=INNOVATION LLC
+example: food=vagan
+example: zip=28855
+
+------------------------------------------------------------------
+To print out a single untruncated record:
+id=value
+
+example: id=1569145
+
+------------------------------------------------------------------
+type "help" to display this
+type "quit" to exit the program
+```
+sample input and results"
+```
+food=vegan
+id       | name                             | type       | location                                         | address          | status     | food                                             | zipcode
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1569145  | Casita Vegana                    | Truck      | JOHN MUIR DR: LAKE MERCED BLVD to SKYLINE BLVD ( | Assessors Block  | APPROVED   | Coffee: Vegan Pastries: Vegan Hot Dogs: Vegan Ta | 64    
+id=1569145
+1569145 | Casita Vegana | Truck | JOHN MUIR DR: LAKE MERCED BLVD to SKYLINE BLVD (200 - 699) | Assessors Block 7283/Lot004 | APPROVED | Coffee: Vegan Pastries: Vegan Hot Dogs: Vegan Tamales: Te: Vegan Shakes | 64
+```
+
+
+```
+id=1181504
+1181504 | John's Catering #5 | Truck | HARRISON ST: 16TH ST \ TREAT AVE to 17TH ST (2000 - 2099) | 2010 HARRISON ST | EXPIRED | Cold Truck: Soda:Chips:Candy: Cold/Hot Sandwiches: Donuts.  (Pitco Wholesale) | 28855
+```
+
+## Note
+This simple program does not make use all data fields. Only these 7 fields are used:
+- locationid (id)
+- Applicant (application)
+- FacilityType (type)
+- LocationDescription (location)
+- Address (address)
+- Status (status)
+- FoodItems (food)
+- Zip Codes (zip)
+
+All searches are single field only (case in sensitive).  
+Search `id=xxxxx` will display a single non trucated data.  
+All other searches will display potentional trucated data for easy display purpose
+
+
+
---
+++ main.py
@@ -0,0 +1,112 @@
+import re
+import sys
+
+# h - headers of displaying column 
+h = ['id', 'name', 'type', 'location', 'address', 'status', 'food', 'zipcode']
+
+# p - position of columns that we are interested
+p = [0, 1, 2, 4, 5, 10, 11, 28]
+
+# w - width of displaying of the columns
+w = [8, 32 , 10, 48, 16, 10, 48, 5 ]
+
+# in memory db
+dataRecords = []
+
+'''
+  import (selective) data from the csv file into a list of fixed length list of strings 
+'''
+def importData(csvFileName):
+  import csv
+  with open(csvFileName, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    lines = []
+    for row in csv_reader:
+      lines.append([row['locationid'], row['Applicant'], row['FacilityType'], row['LocationDescription'], row['Address'], row['Status'], row['FoodItems'], row['Zip Codes'] ] )
+  return lines
+
+'''
+  display info and collect user import query
+'''
+def prompts():
+  print('')
+  print('Use the following format to query food truck data (case insensetive):')
+  print('search format: field=value ')
+  print('at present only support these seven searchable fields: name, type, location, address, status, food, zip')
+  print('')
+  print('example: name=INNOVATION LLC')
+  print('example: food=vagan')
+  print('example: zip=28855')
+  print('')
+  print('------------------------------------------------------------------')
+  print('To print out a single untruncated record:')
+  print('id=value')
+  print('')
+  print('example: id=1569145')
+  print('')
+  print('------------------------------------------------------------------')
+  print('type "help" to display this')
+  print('type "quit" to exit the program')
+  print('')
+  
+'''
+  read command and run search
+'''
+def doSearch():
+  s = input()  
+  # quit if instructed
+  if (s == 'quit'):
+    sys.exit()
+  
+  # help if instructed
+  if (s == 'help'):
+    prompts()
+    return
+  # validate input basic format
+  kv = s.split("=")
+  if (len(kv) != 2):
+    print('invalid command')
+    return
+  # validate input basic format
+  k = kv[0].strip()
+  v = kv[1].strip()
+  if not k in h:
+    print(f'"{k}" is not one of the seven supported filed.')
+    return
+  if k == 'id':
+    locateSingleRecordWithId(v)
+  else:
+    displayQueryResults(k, v)
+
+def locateSingleRecordWithId(id_value):
+  results = [x for x in dataRecords if x[0]==id_value]
+  if not results:
+    print (f'id with "{id_value}" does not exist' )
+    print('')
+    return
+  print (' | '.join(results[0]))
+
+def displayQueryResults(k, v):
+  print(f'{h[0]:{w[0]}} | {h[1]:{w[1]}} | {h[2]:{w[2]}} | {h[3]:{w[3]}} | {h[4]:{w[4]}} | {h[5]:{w[5]}} | {h[6]:{w[6]}} | {h[7]:{w[7]}}')
+  print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+  index = h.index(k)
+  count = 0
+  for x in dataRecords:
+    if re.search(v, x[index], re.IGNORECASE):
+      print(f'{x[0]:<{w[0]}} | {x[1][:w[1]]:<{w[1]}} | {x[2]:<{w[2]}} | {x[3][:w[3]]:<{w[3]}} | {x[4][:w[4]]:<{w[4]}} | {x[5]:<{w[5]}} | {x[6][:w[6]]:<{w[6]}} | {x[7]:<{w[7]}} ' )
+      count = count+1
+  if count==0 :
+    print(f'{k} = {v} does not exist')
+    
+
+# this is the man entry 
+if __name__ == "__main__":
+  dataRecords=importData('foodtruck.csv')
+  prompts()
+  while True:
+    doSearch()
+
+
+
+
+
